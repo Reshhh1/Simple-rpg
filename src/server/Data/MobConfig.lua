9b65cf315befd2b992645c1fb6aefca775d2ccd4
Reshhh1 <reshwanbarhoe1@gmail.com>
2023-09-11 21:42:18 +0200

    Moved file location

---
+++ src/server/Data/MobConfig.lua
@@ -0,0 +1,30 @@
+local module = {
+	["Boemba"] = {
+		tier = 1,
+		Exp = 50,
+		minLevel = 1,
+		maxLevel = 10,
+
+		-- Media
+		Animations = {
+			Attack = 14182920076,
+			Walking = 14211569269
+		},
+		Sounds = {
+			Death = 935843979,
+			Hit = 5501600429,
+			Jump = 8025268823
+		},
+
+		-- Drops
+		Drops = {
+			Weapons = {
+				"Stonebane"
+			},
+			Other = {
+				
+			}
+		}
+	}
+}
+return module
